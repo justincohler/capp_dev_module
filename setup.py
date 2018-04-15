3872f6b6727820903a6d1f586f0cc7a7bd415dd4
Justin Cohler <justincohler@gmail.com>
2018-04-15 11:45:36 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Setup the CAPP Demo.
+
+@author: Justin Cohler
+"""
+from setuptools import setup, find_packages
+
+setup(name='capp-demo',
+      version='0.0.1',
+      description='',
+      author='Justin Cohler',
+      author_email='',
+      url='',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      packages=find_packages(),
+     )
